c855f38f9e3a80451b82d8f9934c3b4283ebf91f
OzodbekUSTECH <126599107+OzodbekUSTECH@users.noreply.github.com>
2023-06-09 19:18:44 +0500

    modified ajax and errors

--- app/forms.py
+++ app/forms.py
@@ -7,20 +7,6 @@ class ApplicationForm(forms.ModelForm):
     phone_number = forms.CharField(label="Номер телефона", max_length=30, widget=forms.TextInput(attrs={'class': 'form-control'}), required=False)
     Comment = forms.CharField(label="Ваше сообщение", widget=forms.Textarea(attrs={'class': 'form-control', 'cols': 30, 'rows': 10}), required=False)
 
-    def clean(self):
-        cleaned_data = super().clean()
-        name = cleaned_data.get('name')
-        email = cleaned_data.get('email')
-        phone_number = cleaned_data.get('phone_number')
-
-        if not name:
-            self.add_error('name', 'Поле "ФИО" должно быть заполнено.')
-
-        if not email:
-            self.add_error('email', 'Поле "Email" должно быть заполнено.')
-
-        if not phone_number:
-            self.add_error('phone_number', 'Поле "Номер телефона" должно быть заполнено.')
 
     class Meta:
         model = Application
