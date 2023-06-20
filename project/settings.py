bf132c38e3ab0d1bbbb68d8f3fda1762b487d729
VladlenKhan <gitvlad777@gmail.com>
2023-06-21 03:02:38 +0500

    changeset

--- project/settings.py
+++ project/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^lb&+p&(xyz%-1pv*dq@e^s@v3zbqt%@_pohsemjgxj9$wi+%y'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
